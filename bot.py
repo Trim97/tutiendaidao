ae5c5bc011c98eabb95cf56fcd6abb6b60747164
Chris Luu <137862042+Trim97@users.noreply.github.com>
2026-03-16 07:56:45 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,12 +18,14 @@ async def chat(update,context):
 
         await update.message.reply_text("Hệ thống đã ghi nhận lời nói của tu sĩ.")
 
-def main():
+async def main():
 
-    app=ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-    app.add_handler(MessageHandler(filters.TEXT,chat))
+    await app.bot.delete_webhook(drop_pending_updates=True)
+
+    app.add_handler(MessageHandler(filters.TEXT, chat))
 
     print("BOT RUNNING")
 
-    app.run_polling()
+    await app.run_polling()
